c35bb8681ca339c497cf9f194ed2bdd4beb30d57
raja k <rajauma530@gmail.com>
2026-08-10 10:15:17 +0530

    fix: optimize Express backend configuration for Vercel deployment 1.3

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -15,7 +15,7 @@ function History() {
   useEffect(() => {
     const fetchHistory = async () => {
       try {
-        const response = await fetch("https://vercel.app");
+        const response = await fetch("https://bulk-mail-hqnw.vercel.app/history");
 
         if (!response.ok) {
           throw new Error("Failed to fetch history");
